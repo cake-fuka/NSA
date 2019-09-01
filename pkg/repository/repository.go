eec0d8756f06eb13f6126b2d1eee6442913a13cd
cake-fuka <blackcat4474@gmail.com>
2019-09-01 10:11:53 +0900

    add struct

--- pkg/repository/repository.go
+++ pkg/repository/repository.go
@@ -11,47 +11,51 @@ import (
 type AvGoleCollections struct {
 	Success  bool `json:"success"`
 	Response struct {
-		HasMore          bool `json:"has_more"`
-		TotalCollections int  `json:"total_collections"`
-		CurrentOffset    int  `json:"current_offset"`
-		Limit            int  `json:"limit"`
-		Collections      []struct {
-			ID            string `json:"id"`
-			Title         string `json:"title"`
-			Keyword       string `json:"keyword"`
-			CoverURL      string `json:"cover_url"`
-			TotalViews    int    `json:"total_views"`
-			VideoCount    int    `json:"video_count"`
-			CollectionURL string `json:"collection_url"`
-		} `json:"collections"`
+		HasMore          bool             `json:"has_more"`
+		TotalCollections int              `json:"total_collections"`
+		CurrentOffset    int              `json:"current_offset"`
+		Limit            int              `json:"limit"`
+		Collections      []CollectionItem `json:"collections"`
 	} `json:"response"`
+}
+
+type CollectionItem struct {
+	ID            string `json:"id"`
+	Title         string `json:"title"`
+	Keyword       string `json:"keyword"`
+	CoverURL      string `json:"cover_url"`
+	TotalViews    int    `json:"total_views"`
+	VideoCount    int    `json:"video_count"`
+	CollectionURL string `json:"collection_url"`
 }
 
 type Video struct {
 	Success  bool `json:"success"`
 	Response struct {
-		HasMore          bool `json:"has_more"`
-		TotalCollections int  `json:"total_collections"`
-		CurrentOffset    int  `json:"current_offset"`
-		Limit            int  `json:"limit"`
-		Videos           []struct {
-			VID         string `json:"vid"`
-			UID         string `json:"uid"`
-			Title       string `json:"title"`
-			Keyword     string `json:"keyword"`
-			Channel     string `json:"channel"`
-			Duration    int    `json:"duration"`
-			Framerate   int    `json:"framerate"`
-			HD          bool   `json:"hd"`
-			AddTime     int    `json:"addtime"`
-			ViewNumber  int    `json:"viewnumber"`
-			Likes       int    `json:"likes"`
-			DisLikes    int    `json:"dislikes"`
-			VideoURL    string `json:"video_url"`
-			EmbeddedURL string `json:"embedded_url"`
-			PreviewURL  string `json:"preview_url"`
-		} `json:"videos"`
+		HasMore          bool        `json:"has_more"`
+		TotalCollections int         `json:"total_collections"`
+		CurrentOffset    int         `json:"current_offset"`
+		Limit            int         `json:"limit"`
+		Videos           []VideoItem `json:"videos"`
 	} `json:"response"`
+}
+
+type VideoItem struct {
+	VID         string `json:"vid"`
+	UID         string `json:"uid"`
+	Title       string `json:"title"`
+	Keyword     string `json:"keyword"`
+	Channel     string `json:"channel"`
+	Duration    int    `json:"duration"`
+	Framerate   int    `json:"framerate"`
+	HD          bool   `json:"hd"`
+	AddTime     int    `json:"addtime"`
+	ViewNumber  int    `json:"viewnumber"`
+	Likes       int    `json:"likes"`
+	DisLikes    int    `json:"dislikes"`
+	VideoURL    string `json:"video_url"`
+	EmbeddedURL string `json:"embedded_url"`
+	PreviewURL  string `json:"preview_url"`
 }
 
 // AvgleからVideo Collectionを取得
